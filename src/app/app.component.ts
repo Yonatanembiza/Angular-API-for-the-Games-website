92158eab18527fe3cdc553b623b81ebecce076ea
Yonatan Embiza Tadesse <enyonathan@gmail.com>
2024-07-31 18:32:33 -0500

    git init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-app';
+  names: string[]= ['Yonatan', 'Daniel', 'Kiniena', 'Tahguas', 'Yisaq'];
+
+  onClickButton(){
+    this.title = "button clicked";
+  }
+
+  students= [{name: "Jack", course: "MWA", gpa: 3.0},
+    {name: "Jill", course: "MPP", gpa: 3.4},
+    {name: "Jim", course: "MWA", gpa: 2.8}];
+
 }
+
